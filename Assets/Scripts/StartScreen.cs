d209e129fef50580065067562afc53a93b3ac95f
NatLark <natalie.larksukthom@student.allenisd.org>
2022-05-07 22:19:56 -0500

    Added a quit button to the main menu

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -9,4 +9,10 @@ public class StartScreen : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log ("QUIT!");
+        Application.Quit();
+    }
 }
