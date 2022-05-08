b01f44abceb2a5cdaf73e917596058978d001abb
SimplyBernieRaye <bernadette.cruz@student.allenisd.org>
2022-05-07 19:34:19 -0500

    Removed files, added dialogue box UI -- NOT WORKING
    我想死。

---
+++ Assets/Scripts/Fudge.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fudge : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            } else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player In Range");
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player Left Range");
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
